4a59efeaf49899a0ab43378bbea7dbde65b9890d
Luis Weir <luisweir@Luiss-MacBook-Pro.local>
2019-08-23 14:05:18 +0100

    added base code

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # zipkin-rp-issue
 
-This project reproduces an issue when instrumenting request-promise.
+This project reproduces an issue when instrumenting request-promise using [zipkin-js](https://github.com/openzipkin/zipkin-js) and [zipkin-instrumentation-request-promise](https://github.com/openzipkin/zipkin-js/tree/master/packages/zipkin-instrumentation-request-promise).
 
-To reproduce the issue follow this steps:
+## Issue description
 
-1. Start the local `docker-compose` environment that contains *zipkin* and a `service mock`.
+The node application in this project serves one endpoint with two methods: `[GET] /` and `[POST] /`. Calls to both methods are forwarded to a [httpbin](https://httpbin.org/) backend service acting echo server.
+
+The issue occurs a given call (e.g. a call to `[POST] /`) takes longer to be routed to the backend than a newer call (e.g. a call to `[GET] /`).
+
+In short, the issue is that the *span* generated for the slowest call by the instrumented `request-promise` invocation, takes the `parentId` of the newer call (`[GET] /` and not the one from the previous call (`[POST] /`).
+
+## Reproduce
+
+Follow this steps to reproduce the error:
+
+1. Start up the local environment containing *zipkin* and the *backend httpbin service* using `docker-compose` as following.
 
 ```bash
 docker-compose up -d
--- server.js
+++ server.js
@@ -1,18 +1,106 @@
-var express     =   require("express");
-var app         =   express();
-var bodyParser  =   require("body-parser");
-var router      =   express.Router();
+const express     =   require("express");
+const server      =   express();
+const bodyParser  =   require("body-parser");
+const router      =   express.Router();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({"extended" : false}));
+/////////////////////////////// Common
+PORT = 3000;
+TARGET = "http://localhost:8080/anything";
 
+/////////////////////////////// Zipkin
+const localServiceName = "service";
+const zipkinServer = "localhost";
+const zipkinPort= "9411";
+const zipkinSamplingRate = 1;
+
+const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
+const {wrapRequest} = require('zipkin-instrumentation-request-promise');
+const { Tracer, BatchRecorder, sampler, jsonEncoder: {JSON_V2} } = require('zipkin');
+const {HttpLogger} = require('zipkin-transport-http');
+const CLSContext = require('zipkin-context-cls');
+const ctxImpl = new CLSContext('zipkin');
+
+// base URL
+const zipkinBaseUrl = `http://${zipkinServer}:${zipkinPort}`;
+console.log(`Setting zipkin to: ${zipkinBaseUrl} with zipkinSamplingRate=${zipkinSamplingRate}`);
+
+const httpLogger = new HttpLogger({
+  endpoint: `${zipkinBaseUrl}/api/v2/spans`,
+  jsonEncoder: JSON_V2
+});
+
+// This is a hack that lets you see the data sent to Zipkin!
+
+function recorder() {
+
+  const logger = {
+    logSpan: (span) => {
+      const json = JSON_V2.encode(span);
+      console.log(`reporting: ${json}`);
+      httpLogger.logSpan(span);
+    }
+  };
+
+  const batchRecorder = new BatchRecorder({logger});
+
+  // This is a hack that lets you see which annotations become which spans
+  return ({
+    record: (rec) => {
+      const {spanId, traceId} = rec.traceId;
+      console.log(`recording: ${traceId}/${spanId} ${rec.annotation.toString()}`);
+      batchRecorder.record(rec);
+    }
+  });
+}
+
+const tracer = new Tracer({
+  ctxImpl, 
+  recorder: recorder(), 
+  sampler: new sampler.CountingSampler(zipkinSamplingRate),
+  localServiceName
+});
+
+/////////////////////////////// Express middlewares
+server.use( bodyParser.json() );
+server.use( bodyParser.urlencoded({"extended" : false}) );
+server.use( zipkinMiddleware({tracer}) );
+
+/////////////////////////////// Invoker
+class Invoker {
+    constructor(){}
+    
+    async call(url, options){
+        //prepare the zipkin request-promise wrapper
+        let remoteServiceName = url.split("/")[2].split(":")[0];
+        let request = new wrapRequest(tracer, remoteServiceName);
+        options.json = true;
+        console.log("calling : " + url + " with options: " + JSON.stringify(options));
+        return await request(url, options);
+    }
+}
+  
+const invoker = new Invoker();
+
+
+/////////////////////////////// Routes
 router.get("/",function(req,res){
-    res.json({"method" : "GET", "URL":req});
+    //set the call options
+    let options = {
+        method: req.method,
+        headers: req.headers
+      };
+    res.json( invoker.call(TARGET, options) );
 });
 
 router.post("/",function(req,res){
-    res.json({"method" : "GET", "URL":req});
+    //set the call options
+    let options = {
+        method: req.method,
+        headers: req.headers,
+        body: req.body
+      };
+    res.json( invoker.call(TARGET, options) );
 });
 
-app.listen(3000);
-console.log("Listening to PORT 3000");
+server.listen(PORT);
+console.log(`Listening to PORT ${PORT}`);
